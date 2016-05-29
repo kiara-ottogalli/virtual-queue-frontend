d98f525edb623cfe3bdf8d6a36d7600f3b811639
Kiara Ottogalli <kiara.ottogalli@gmail.com>
2016-05-28 20:01:23 -0400

    Just after scaffolding the angular app with yo angular, and installing node module phantomjs. Excluded files in node_modules and bower_components.

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name virtualQueueFrontendApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the virtualQueueFrontendApp
+ */
+angular.module('virtualQueueFrontendApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
